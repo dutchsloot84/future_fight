950071110e92049e4790c73e3e10fc3236c1f74e
Dutchsloot <dutchsloot84@gmail.com>
2016-06-30 14:52:44 -0700

    Updated mission_script_windows
    Added logic to run missions while the lower energy prompt does not exist

--- mission_script_windows.sikuli/mission_script_windows.py
+++ mission_script_windows.sikuli/mission_script_windows.py
@@ -1,13 +1,28 @@
-x = 0
-# need screen for low energy, lowEnergy = find(???)
+lowEnergy = exists("1467321494644.png")
 missionEnd = 0
-while x <= 35:
+
+def rechargeEnergyIfFree():
+    if exists("1467322124639.png", 5):
+        click("1467322124639.png")
+    if exists("1467322223525.png", 15):
+        click("1467322223525.png")
+    if exists("1467322499864.png", 15):
+        click("1467322499864.png")
+    if exists("1467323163474.png", 10):
+        click("1467323163474.png")
+        sleep(3)
+        type(Key.ESC)
+
+while not lowEnergy:
     exists("1467144561903.png", 15)
     click("1467144561903.png")
+    if exists("1467321494644.png", 5):
+        #Break out of loop due to low energy
+        break
     while missionEnd <= 26:
-        sleep(10)
         if exists("1467144845807.png", 10):         
             #Check for Shield Lvl prompt
+            #TODO: Create a prompt check function
             if exists("1467154405580.png", 5):
                 click("1467154405580.png")
             #Check for Dimension Rift prompt
@@ -26,4 +41,5 @@ while x <= 35:
     if exists("1467154405580.png", 15):
         click("1467154405580.png")
 x+=1
-    
+
+rechargeEnergyIfFree()
